2fe51ceadba008722f649f5b87b5e40bf02347e7
jarcane <jarcane@users.noreply.github.com>
2014-11-11 15:05:14 +0200

    Saving DIM for posterity.
    I'm actually about to axe it. Want this here in case I change my mind.
    
    Heresy just went FP, and DIM really has no place in dynamic languages
    anyway.

--- heresy/main.rkt
+++ heresy/main.rkt
@@ -1,9 +1,14 @@
 #lang racket
 
-;; heresy - a BASIC-flavored Lisp dialect
+;; Heresy - a BASIC-flavored Lisp dialect
 ; Copyright (C) 2014 John S. Berry III
 ; Licensed with the LGPL v.3.0
 
+;; Provides
+
+;; Support Functions
+
+; REPL
 
 ;                                                    
 ;                                                    
@@ -24,7 +29,14 @@
 
 ;; Declarations
 
-; DIM
+; DIM name ['(length)]
+; Defines a variable name with null contents '(), or length #f values
+(define-syntax dim
+  (syntax-rules ()
+    [(_ name) (define name '())]
+    [(_ name '(length)) 
+     (define name (for/list ([i (in-range length)])
+                    #f))]))
 
 ; LET
 
@@ -117,6 +129,3 @@
 ; True
 
 ; False
-
-; Nil
-
