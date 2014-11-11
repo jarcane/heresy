296e9022053abb569725d79a93b12b31f11520c7
jarcane <jarcane@users.noreply.github.com>
2014-11-11 11:45:25 +0200

    Outlining the basic functions
    Setting up a basic list of primitives to provide in Heresy.

---
+++ heresy/main.rkt
@@ -0,0 +1,122 @@
+#lang racket
+
+;; heresy - a BASIC-flavored Lisp dialect
+; Copyright (C) 2014 John S. Berry III
+; Licensed with the LGPL v.3.0
+
+
+;                                                    
+;                                                    
+;                                                    
+;   ;;;;;;       ;;       ;;;;      ;;;;      ;;;;   
+;     ;   ;;    ;  ;    ;;    ;;     ;;     ;;    ;; 
+;     ;   ;;    ;  ;    ;;    ;;     ;;     ;;    ;; 
+;     ;   ;;  ;;    ;;    ;          ;;     ;;       
+;     ;;;;    ;;    ;;     ;;        ;;     ;;       
+;     ;;;;    ;;    ;;     ;;        ;;     ;;       
+;     ;   ;;  ;;;;;;;;       ;       ;;     ;;       
+;     ;   ;;  ;;    ;;  ;;    ;;     ;;     ;;    ;; 
+;     ;   ;;  ;;    ;;  ;;    ;;     ;;     ;;    ;; 
+;   ;;;;;;    ;;    ;;    ;;;;      ;;;;      ;;;;   
+;                                                    
+;                                                    
+;                                                    
+
+;; Declarations
+
+; DIM
+
+; LET
+
+; DEF
+
+;; Flow Control
+
+; IF
+
+; FOR
+
+; DO
+
+; SELECT
+
+;; I/O
+
+; PRINT
+
+; INPUT
+
+;; Math
+
+; # (infix operator)
+
+; RND
+
+;; Miscellaneous
+
+; REM
+
+
+;                                  
+;                                  
+;                                  
+;                                  
+;   ;;;      ;;;;;   ;;; ;  ;;;;;  
+;    ;         ;    ;   ;;   ;   ; 
+;    ;         ;    ;        ;   ; 
+;    ;         ;     ;;;;    ;   ; 
+;    ;         ;         ;   ;;;;  
+;    ;   ;     ;         ;   ;     
+;    ;   ;     ;    ;;   ;   ;     
+;   ;;;;;;   ;;;;;  ; ;;;   ;;;    
+;                                  
+;                                  
+;                                  
+;                                  
+
+;; Lists
+
+; CONS (JOIN?)
+
+; HEAD
+
+; TAIL
+
+;; Predicates
+
+; LIST?
+
+; ATOM? 
+
+; NULL?
+
+; ZERO?
+
+; EQ?
+
+; =?
+
+;; Functions/Macros
+
+; LAMBDA (FN?)
+
+; MACRO 
+
+; EVAL
+
+; QUOTE
+
+;; Boolean
+
+; AND
+
+; OR
+
+; NOT
+
+; True
+
+; False
+
+; Nil
+
---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define name "heresy")
+(define collection "heresy")
+(define blurb
+  "A BASIC-Flavored Lisp dialect")
+(define primary-file "/heresy/main.rkt")
+(define categories '(devtools metaprogramming))
