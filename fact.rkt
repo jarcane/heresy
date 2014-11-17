292c89ab22ce1a50574bfd90c52d7e04bd6999dd
jarcane <jarcane@users.noreply.github.com>
2014-11-17 20:39:40 +0200

    Additional Factorial example
    Showing off the for w/carry function.

--- fact.rkt
+++ fact.rkt
@@ -1,6 +1,16 @@
 #lang s-exp heresy
 
+; Classical recursive
 (def fn fact (n)
   (select
    ((zero? n) 1)
    (else (* n (fact (- n 1))))))
+
+; W/ Heresy's for w/carry
+(def fn fact-2 (n)
+  (for (x in (range n to 1 step -1))
+    (if (null? cry) 
+     then
+       (carry x)
+     else
+       (carry (* cry x)))))
