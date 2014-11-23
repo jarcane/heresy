67840441b6520082100e6ab5326dec3baa1828a7
jarcane <jarcane@users.noreply.github.com>
2014-11-23 21:19:24 +0200

    mid$ implemented.

--- lib/string.rkt
+++ lib/string.rkt
@@ -11,3 +11,14 @@
 ; Takes a list of strings and returns a single string concatenated together
 (def fn list& (lst)
   (apply & lst))
+
+; (mid$ *str* *idx* *len*)
+; Returns a slice of str, len long, starting at idx
+(def fn mid$ (str idx length)
+  (select
+   ((> idx (len$ str)) (error 'mid$ "out of index"))
+   ((> (+ idx length) (len$ str)) (error 'mid$ "slice too long"))
+   (else (list& 
+          (reverse 
+           (for (x in (range idx to (+ idx length -1)))
+             (carry (join (index x (list$ str)) cry))))))))
