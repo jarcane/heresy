2a27a33f33df664f21fc91f2ac2e4815a63df71a
jarcane <jarcane@users.noreply.github.com>
2014-11-24 15:26:58 +0200

    head$, tail$, empty?, and instr implemented

--- lib/string.rkt
+++ lib/string.rkt
@@ -2,6 +2,11 @@
 
 (require "list.rkt")
 
+; (empty? str)
+; Returns true if string is empty)
+(def fn empty? (str)
+  (=$ str ""))
+
 ; (len$ *str*)
 ; Returns the length of the given str
 (def fn len$ (str)
@@ -11,6 +16,19 @@
 ; Takes a list of strings and returns a single string concatenated together
 (def fn list& (lst)
   (apply & lst))
+
+; (head$ *str*)
+; Returns the first character of the string
+(def fn head$ (str)
+  (head (list$ str)))
+
+; (tail$ *str*)
+; Returns the remainder of the string
+(def fn tail$ (str)
+  (let ([tl (list$ str)])
+    (if (null? tl) then
+        ""
+        else (list& (tail tl)))))
 
 ; (left$ *str* *n* )
 ; Returns a slice of str, length long, starting at idx
@@ -31,3 +49,12 @@
 ; Returns a slice of the string from start to finish, inclusive
 (def fn slice$ (str (start 0) (finish (len$ str)))
   (list& (slice (list$ str) start finish)))
+
+; (instr *str* *search*)
+; Returns the index of the first instance given search string within str, or #f
+(def fn instr (str search (idx 1))
+  (select 
+   ((empty? str) False)
+   ((> (len$ search) (len$ str)) False)
+   ((=$ search (left$ str (len$ search))) idx)
+   (else (instr (tail$ str) search (+ 1 idx)))))
