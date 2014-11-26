cf0704462d92c87fe4d6521ec955462c57a1451e
jarcane <jarcane@users.noreply.github.com>
2014-11-26 21:49:13 +0200

    Added compose.
    This is a simple, binary function composition operator.

--- lib/theory.rkt
+++ lib/theory.rkt
@@ -7,10 +7,16 @@
 ; The Y-combinator
 (def Y
   (fn (b)
-    ((fn (f) (b (fn (x) ((f f) x))))
-     (fn (f) (b (fn (x) ((f f) x)))))))
+      ((fn (f) (b (fn (x) ((f f) x))))
+       (fn (f) (b (fn (x) ((f f) x)))))))
 
 ; (partial *fun* *init-args* ...)
 ; Returns a new function with with init-args partially applied to fun
 (def fn partial (fun . rest)
   (fn (x . args) (apply fun (append rest (append (list x) args)))))
+
+; (compose *fun* *fun2*)
+; Returns a new function which is the composition of the two, 
+; returning the result of fun on the evaluation of fun2 and it's args. 
+(def fn compose (fun fun2)
+  (fn (x . args) (fun (apply fun2 (join x args)))))
