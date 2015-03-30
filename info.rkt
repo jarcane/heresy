5ccd9589868238f593aa17d3e34d02b9b549ee00
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-03-30 09:27:37 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -4,3 +4,6 @@
 (define blurb
   "A BASIC-Flavored Lisp dialect")
 (define scribblings '(["docs/heresy.scrbl" ()]))
+
+(define deps '("base"))
+(define build-deps '("rackunit-lib" "sandbox-lib" "scribble-lib"))
