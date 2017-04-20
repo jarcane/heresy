f247e01b7b0ea76a74c95e691b1161fbcb8a481b
jarcane <jarcane@users.noreply.github.com>
2017-04-20 19:28:38 +0300

    Add error checking and safety to updates

--- lib/hole.rkt
+++ lib/hole.rkt
@@ -1,6 +1,7 @@
 #lang heresy
 
 (import rkt racket)
+(import rkt racket/exn)
 
 (describe Signal
           (type Null)
@@ -18,8 +19,16 @@
          ((get) (do
                   (rkt:thread-send (signal 'thread-to-send) cry)
                   (carry cry)))
-         ((reset) (carry (signal 'new-val)))
-         ((update) (carry (apply (signal 'update-fn) cry (signal 'args))))
+         ((reset) (do
+                    (rkt:thread-send (signal 'thread-to-send) (signal 'new-val))
+                    (carry (signal 'new-val))))
+         ((update) (rkt:with-handlers ((rkt:exn:fail?
+                                        (fn (e)
+                                            (rkt:thread-send (signal 'thread-to-send) e)
+                                            (carry cry))))
+                      (def result (apply (signal 'update-fn) cry (signal 'args)))
+                      (rkt:thread-send (signal 'thread-to-send) result)
+                      (carry result)))
          (else (carry cry)))))))
 
 (def fn deref (a)
@@ -27,7 +36,12 @@
   (rkt:thread-receive))
 
 (def fn reset (a new-val)
-  (rkt:thread-send a (Signal `(reset ,new-val))))
+  (rkt:thread-send a (Signal `(reset ,new-val)))
+  (rkt:thread-receive))
 
 (def fn update (a fn . args)
-  (rkt:thread-send a (Signal `(update * * ,fn ,args))))
+  (rkt:thread-send a (Signal `(update * * ,fn ,args)))
+  (def result (rkt:thread-receive))
+  (if (rkt:exn:fail? result) then
+      (rkt:raise result) else
+      result))
