00a9662c3fbc5b14cb2e8e243adb1f0fc21b5358
John Berry <jarcane@users.noreply.github.com>
2017-04-20 10:04:12 +0300

    basic one-arg update

--- lib/atom.rkt
+++ lib/atom.rkt
@@ -5,7 +5,8 @@
 (describe Signal
           (type Null)
           (new-val Null)
-          (thread-to-send (rkt:current-thread)))
+          (thread-to-send (rkt:current-thread))
+          (update-fn (fn (x) x)))
 
 (def fn atom (val)
   (rkt:thread
@@ -17,11 +18,15 @@
                      (rkt:thread-send (signal 'thread-to-send) cry)
                      (carry cry)))
             ((reset) (carry (signal 'new-val)))
+            ((update) (carry ((signal 'update-fn) cry)))
             (else (carry cry)))))))
 
 (def fn deref (a)
   (rkt:thread-send a (Signal `(get)))
   (rkt:thread-receive))
 
-(def fn reset (a val)
-  (rkt:thread-send a (Signal `(reset ,val))))
+(def fn reset (a new-val)
+  (rkt:thread-send a (Signal `(reset ,new-val))))
+
+(def fn update (a fn)
+  (rkt:thread-send a (Signal `(update * * ,fn))))
