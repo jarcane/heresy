b2b3d4401680ef6fd104b60c4f2cb81dfcd96ff9
jarcane <jarcane@users.noreply.github.com>
2017-04-22 10:44:27 +0300

    add hole documentation commenets

--- lib/hole.rkt
+++ lib/hole.rkt
@@ -4,6 +4,8 @@
 (import rkt racket)
 (provide (all-defined-out))
 
+; Signal
+; A helper data type for signal passing to hole threads
 (describe Signal
           (type Null)
           (new-val Null)
@@ -11,6 +13,10 @@
           (update-fn (fn (x) x))
           (args Null))
 
+; (hole *val*)
+; Any -> Hole
+; Returns a hole containing val. Holes are tiny in-memory databases that carry a single value,
+; and communicate by passing messages to and from the thread.  
 (def fn hole (val)
   (rkt:thread
    (fn ()
@@ -32,14 +38,24 @@
                       (carry result)))
          (else (carry cry)))))))
 
+; (deref *hol*)
+; Hole -> Any
+; Returns the current value store in hol.
 (def fn deref (hol)
   (rkt:thread-send hol (Signal `(get * ,(rkt:current-thread))))
   (rkt:thread-receive))
 
+; (reset *hol* *new-val*)
+; Hole Any -> Hole
+; Resets the current value of hol to new-val
 (def fn reset (hol new-val)
   (rkt:thread-send hol (Signal `(reset ,new-val)))
   hol)
 
+; (update *hol* *fn* . *args* ...)
+; Hole Fn . Args -> Hole
+; Updates the current value of hol by applying fn with the current value as first arg, and args
+; as the remaining arguments
 (def fn update (hol fn . args)
   (rkt:thread-send hol (Signal `(update * ,(rkt:current-thread) ,fn ,args)))
   (def result (rkt:thread-receive))
