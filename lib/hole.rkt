b0dacf21da1f8c7080d8c9288a96f1da7f1e6f48
jarcane <jarcane@users.noreply.github.com>
2017-04-23 23:26:04 +0300

    not working: blocks after first reset

--- lib/hole.rkt
+++ lib/hole.rkt
@@ -9,47 +9,55 @@
 (describe Signal
           (type Null)
           (new-val Null)
-          (thread-to-send (rkt:current-thread))
           (update-fn (fn (x) x))
           (args Null))
+
+; Hole
+; Core data type for holes. Not especially useful on its own, except as a reference for is-a?.
+(describe Hole
+          (thread Null)
+          (channel Null))
 
 ; (hole *val*)
 ; Any -> Hole
 ; Returns a hole containing val. Holes are tiny in-memory databases that carry a single value,
 ; and communicate by passing messages to and from the thread.  
 (def fn hole (val)
-  (rkt:thread
-   (fn ()
-     (do loop with val
-       (def signal (rkt:thread-receive))
-       (select case (signal 'type)
-         ((get) (do
-                  (rkt:thread-send (signal 'thread-to-send) cry)
-                  (carry cry)))
-         ((reset) (do
-                    (rkt:thread-send (signal 'thread-to-send) (signal 'new-val))
-                    (carry (signal 'new-val))))
-         ((update) (rkt:with-handlers ((rkt:exn:fail?
-                                        (fn (e)
-                                            (rkt:thread-send (signal 'thread-to-send) e)
-                                            (carry cry))))
-                      (def result (apply (signal 'update-fn) cry (signal 'args)))
-                      (rkt:thread-send (signal 'thread-to-send) result)
-                      (carry result)))
-         (else (carry cry)))))))
+  (def chan (rkt:make-channel))
+  (def thr
+    (rkt:thread
+     (fn ()
+       (do loop with val
+           (def signal (rkt:channel-get chan))
+           (select case (signal 'type)
+                   ((get) (do
+                            (rkt:channel-put chan cry)
+                            (carry cry)))
+                   ((reset) (do
+                              (rkt:channel-put chan (signal 'new-val))
+                              (carry (signal 'new-val))))
+                   ((update) (rkt:with-handlers ((rkt:exn:fail?
+                                                  (fn (e)
+                                                    (rkt:channel-put chan e)
+                                                    (carry cry))))
+                                                (def result (apply (signal 'update-fn) cry (signal 'args)))
+                                                (rkt:channel-put chan result)
+                                                (carry result)))
+                   (else (carry cry)))))))
+  (Hole (list thr chan)))
 
 ; (deref *hol*)
 ; Hole -> Any
 ; Returns the current value store in hol.
 (def fn deref (hol)
-  (rkt:thread-send hol (Signal `(get * ,(rkt:current-thread))))
-  (rkt:thread-receive))
+  (rkt:channel-put (hol 'channel) (Signal `(get)))
+  (rkt:channel-get (hol 'channel)))
 
 ; (reset *hol* *new-val*)
 ; Hole Any -> Hole
 ; Resets the current value of hol to new-val
 (def fn reset (hol new-val)
-  (rkt:thread-send hol (Signal `(reset ,new-val)))
+  (rkt:channel-put (hol 'channel) (Signal `(reset ,new-val)))
   hol)
 
 ; (update *hol* *fn* . *args* ...)
@@ -57,8 +65,8 @@
 ; Updates the current value of hol by applying fn with the current value as first arg, and args
 ; as the remaining arguments
 (def fn update (hol fn . args)
-  (rkt:thread-send hol (Signal `(update * ,(rkt:current-thread) ,fn ,args)))
-  (def result (rkt:thread-receive))
+  (rkt:channel-put (hol 'channel) (Signal `(update * ,fn ,args)))
+  (def result (rkt:channel-get (hol 'channel)))
   (if (rkt:exn:fail? result) then
       (rkt:raise result) else
       hol))
@@ -68,4 +76,15 @@
 ; Expects a Thing stored in hol, and resets the value in the thing field defined by sym to val
 (def macro reset-thing (hol (field val) ...)
   (update hol (fn (t)
-                  (thing extends t (field val) ...))))
+                (thing extends t (field val) ...))))
+
+
+(def foo (hole 1))
+(reset foo 2)
+(deref foo)
+(update foo + 5)
+(deref foo)
+(reset foo (thing (foo 1)))
+((deref foo))
+(reset-thing foo (foo 2))
+((deref foo))
