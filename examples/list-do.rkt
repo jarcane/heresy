48ed7cb98ee7fedf7f461f975c3366a146e50cc7
jarcane <jarcane@users.noreply.github.com>
2017-05-17 22:24:29 +0300

    working guards for list-do experiment

--- examples/list-do.rkt
+++ examples/list-do.rkt
@@ -10,17 +10,29 @@
 (def fn list-bind (lst fn)
   (flatten (map fn lst)))
 
+(def fn list-guard (test)
+  (if test then (list Null) else Null))
+
 (def macroset list-do (= <- if yield)
-  ((_ (exp ...)) (exp ...))  
+  ((_ (yield exp ...)) (list exp ...))  
   ((_ (name = val) exp ...)
    (list-bind (list val) (fn (name) (list-do exp ...))))
   ((_ (name <- val) exp ...)
    (list-bind val (fn (name) (list-do exp ...))))
-  ((_ (if pred? name) exp ...)
-   (list-bind (filter pred? name) (fn (name) (list-do exp ...)))))
+  ((_ (if test) exp ...)
+   (list-bind (list-guard test) (fn (name) (list-do exp ...))))
+  )
 
 (list-do
  (x <- (range 1 to 5))
  (y <- (range 1 to 5))
  (z = (* x y))
- (list z))
+ (if (even? z))
+ (yield z))
+
+(list-do
+ (rank <- (append (range 2 to 10) '(J Q K A)))
+ (suit <- '(♠ ♣ ♥ ♦))
+ (if (equal? suit '♦))
+ (card = (format$ "#_#_" rank suit))
+ (yield card))
