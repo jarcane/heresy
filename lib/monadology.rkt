e0b20a4e179bfcfd22ea222d116c34f1e99a97cf
John Berry <jarcane@users.noreply.github.com>
2017-05-19 13:21:58 +0300

    Generic do notation DSL

---
+++ lib/monadology.rkt
@@ -0,0 +1,18 @@
+#lang s-exp "../private/base.rkt"
+
+(def macroset monad-do (<- := yield if)
+  ((_ (>>= return guard)
+      (yield exp ...))
+   (return exp ...))
+  ((_ (>>= return guard)
+      (exp0 ...) exps ...)
+   (>>= (exp0 ...) (fn (_) (make-do exps ...))))
+  ((_ (>>= return guard)
+      (name <- exp) exps ...)
+   (>>= exp (fn (name) (make-do exps ...))))
+  ((_ (>>= return guard)
+      (name := exp) exps ...)
+   (>>= (return exp) (fn (name) (make-do exps ...))))
+  ((_ (>>= return guard)
+      (if test) exps ...)
+   (>>= (guard test) (fn (_) (make-do exps ...)))))
