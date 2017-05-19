6b68c2ffe359c674a0562c2b6c2f8355454dcdb7
John Berry <jarcane@users.noreply.github.com>
2017-05-19 15:33:46 +0300

    Changes to generic monad do dsl

--- lib/monadology.rkt
+++ lib/monadology.rkt
@@ -1,18 +1,33 @@
 #lang s-exp "../private/base.rkt"
 
+(provide (all-defined-out))
+
 (def macroset monad-do (<- := yield if)
+  
   ((_ (>>= return guard)
       (yield exp ...))
    (return exp ...))
+
   ((_ (>>= return guard)
-      (exp0 ...) exps ...)
-   (>>= (exp0 ...) (fn (_) (make-do exps ...))))
+      (name := exp)
+      exps ...)
+   (>>= (return exp)
+        (fn (name) (monad-do (>>= return guard) exps ...))))
+  
   ((_ (>>= return guard)
-      (name <- exp) exps ...)
-   (>>= exp (fn (name) (make-do exps ...))))
+      (name <- exp)
+      exps ...)
+   (>>= exp
+        (fn (name) (monad-do (>>= return guard) exps ...))))
+  
   ((_ (>>= return guard)
-      (name := exp) exps ...)
-   (>>= (return exp) (fn (name) (make-do exps ...))))
+      (if test)
+      exps ...)
+   (>>= (guard test)
+        (fn (_) (monad-do (>>= return guard) exps ...))))
+
   ((_ (>>= return guard)
-      (if test) exps ...)
-   (>>= (guard test) (fn (_) (make-do exps ...)))))
+      (exp0 ...)
+      exps ...)
+   (>>= (exp0 ...)
+        (fn (_) (monad-do (>>= return guard) exps ...)))))
