ec3ef277150badcca0969035e1b5143247f6863f
jarcane <jarcane@users.noreply.github.com>
2017-05-20 21:50:02 +0300

    Version 0.2.0

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define name "Heresy")
 (define collection "heresy")
-(define version "0.1.0")
+(define version "0.2.0")
 (define blurb
   "A BASIC-Flavored Lisp dialect")
 (define scribblings '(["docs/heresy.scrbl" (multi-page) (language)]))
