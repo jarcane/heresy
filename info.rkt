861a86e71c23a5e8f1db3aaee1cb8b204453c966
Annaia Berry <jarcane@users.noreply.github.com>
2018-04-30 10:59:32 +0300

    Update version in info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define name "Heresy")
 (define collection "heresy")
-(define version "0.2.1")
+(define version "0.3.0")
 (define blurb
   "A BASIC-Flavored Lisp dialect")
 (define scribblings '(["docs/heresy.scrbl" (multi-page) (language)]))
